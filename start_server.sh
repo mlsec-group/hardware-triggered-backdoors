bb4b10a2fb86ffe963074f51294f44d0f6a1b331
Jonas Möller <jonas.moeller.1@tu-berlin.de>
2026-04-29 13:42:14 +0200

    Update

--- start_server.sh
+++ start_server.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-BACKEND_ONE="$1"
-BACKEND_TWO="$2"
-
-if [[ -z "$1" ]]; then
-    echo "error: did not provide first backend name"
-    echo "usage: ./start_server.sh <backend_name_1> <backend_name_2>"
+# Require at least 2 arguments
+if [[ $# -lt 2 ]]; then
+    echo "error: need at least two backend names"
+    echo "usage: ./start_server.sh <backend_name_1> <backend_name_2> [backend_name_3 ...]"
     exit 1
 fi
 
-if [[ -z "$2" ]]; then
-    echo "error: did not provide second backend name"
-    echo "usage: ./start_server.sh <backend_name_1> <backend_name_2>"
-    exit 1
-fi
+BACKENDS=("$@")
+
+# Build argument array
+BACKEND_ARGS=()
+for b in "${BACKENDS[@]}"; do
+    BACKEND_ARGS+=(--backends "$b")
+done
 
 DATASET=imagenet
 
-if [ $DATASET = "imagenet" ]; then
+if [[ "$DATASET" == "imagenet" ]]; then
     DATASET_ARGS="--model_type cnn --model_path models/imagenet/efficientnet_v2_s.pt --dataset imagenet"
     DATASET_ARGS="--model_type cnn --model_path models/imagenet/resnet18_model.pt --dataset imagenet"
     DATASET_ARGS="--model_type cnn --model_path models/imagenet/vit_b_32.pt --dataset imagenet"
@@ -25,8 +25,7 @@ if [ $DATASET = "imagenet" ]; then
 fi
 
 python3 main.py \
-    --backends $BACKEND_ONE \
-    --backends $BACKEND_TWO \
+    "${BACKEND_ARGS[@]}" \
     --port 13370 \
     --http_port 9696 \
     --seed 1230 \
@@ -36,12 +35,12 @@ python3 main.py \
     ${DATASET_ARGS} \
     --model_dtype float32 \
     --n_poison_samples 1 \
-    --n_iterations 5 \
+    --n_iterations 6 \
     --heuristic gradient_sgd \
     --permute_after_gradient false \
     --flip_bits_after_gradient true \
     --n_bits_flipped 5 \
-    --n_samples 10 \
+    --n_samples 100 \
     --do_crossover false \
     --do_one_vs_all false \
     --use_full_model false \
